2a1fcb4e8fec3de8d6afb5d8e0dbf550c1c96878
Victor Owiti <viowiti12@gmail.com>
2022-12-30 20:23:59 +0300

    Added services

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
 
       <Navbar />
       <NewHero />
-      {/* <Hero />
-      <Services /> */}
+      {/* <Hero />*/}
+      <Services />
       <Footer />
 
       <main></main>
