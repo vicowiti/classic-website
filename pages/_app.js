eb5d7df209005dc51f4bf9658b85d9f7d21d57e3
Victor Owiti <101042985+vicowiti@users.noreply.github.com>
2023-02-13 15:48:49 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,14 @@ import "../styles/NewHero.css";
 import "../styles/Menu.css";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
+import Script from "next/script"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NavBar />
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2993150059930953"
+     crossOrigin="anonymous" />
       <Component {...pageProps} />
       <Footer />
     </>
